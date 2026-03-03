fa19941454ab4568eb9abb180359a85e3893b981
Javi <itsjavii1610@gmail.com>
2026-03-03 13:17:32 +0100

    Cosorro

--- Java/src/BizumModel.java
+++ Java/src/BizumModel.java
@@ -1,2 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class BizumModel {
+
+    private int totalAmount;
+    LocalDate today = LocalDate.now();
+    LocalTime now = LocalTime.now();
+    ArrayList<String> transactions;
+
 }
--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,10 @@
+import java.time.LocalTime;
+import java.util.Locale;
+
 public class Main {
     static void main(String[] args) {
+        LocalTime now = LocalTime.now();
 
+        System.out.println(now);
     }
 }
